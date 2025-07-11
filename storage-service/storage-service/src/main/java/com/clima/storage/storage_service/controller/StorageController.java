0a7dad6a6b83588488ea75f303b4e920639e6277
Aaron <aaronrodrigo@hotmail.com>
2025-07-11 06:30:18 -0600

    se adapta a h2

--- storage-service/storage-service/src/main/java/com/clima/storage/storage_service/controller/StorageController.java
+++ storage-service/storage-service/src/main/java/com/clima/storage/storage_service/controller/StorageController.java
@@ -1,12 +1,7 @@
 package com.clima.storage.storage_service.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import com.clima.storage.storage_service.repository.ClimateDataRepository;
 
 import dto.ClimateData;
--- storage-service/storage-service/src/main/java/com/clima/storage/storage_service/repository/ClimateDataRepository.java
+++ storage-service/storage-service/src/main/java/com/clima/storage/storage_service/repository/ClimateDataRepository.java
@@ -1,7 +1,7 @@
 package com.clima.storage.storage_service.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import dto.ClimateData;
 
 public interface ClimateDataRepository extends JpaRepository<ClimateData, Long> {
